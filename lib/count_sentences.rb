3f012a8c4faddfff40170d0398bb1611f9409074
Melissa Lawrence <mlawrence537@gmail.com>
2021-02-24 16:24:04 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1][-1] == "."
+      return true
+    else 
+      return false
+    end
   end
 
   def question?
-
+    if self[-1][-1] == "?"
+      return true
+    else 
+      return false
+    end
   end
 
   def exclamation?
-
+    if self[-1][-1] == "!"
+      return true
+    else 
+      return false
+    end
   end
 
   def count_sentences
-
+    self_split = self.split(/[.!?]/)
+    final_split = self_split.reject {|sentence| sentence.empty?}
+    answer = final_split.count
   end
+
 end
